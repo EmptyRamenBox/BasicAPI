3ca96b30591b219e4562c5f13ca3538f4f28d913
EmptyRamenBox <alexis.martinezo92@gmail.com>
2020-04-07 11:30:57 -0500

    Added Package

--- src/index.js
+++ src/index.js
@@ -3,16 +3,25 @@
 import "./helpers/dotenv";
 
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import pino from "express-pino-logger";
 
 const app = express();
 // All environment variables are always to be strings
 //      We must do "parseInt", to parse the output
 const port = parseInt(process.env.PORT);
 
+// Security Feature Implemented
+app.use(cors({ origin: process.env.ORIGIN }));
+// origin option: "filter by the source"
+app.use(helmet());
+app.use(pino());
+app.use(express.json()); // Allow express to use JSON in the body
+
 // doing request to the root only
 // not to any other places
 app.get("/", (req, res) => {
-  //   res.send("Hello There");
   res.json({ msg: "Hello There" });
 });
 app.listen(port);
